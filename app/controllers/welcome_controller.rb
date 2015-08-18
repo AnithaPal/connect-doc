c4caea62b9bc1c220750096af14fcdbe9ce96f44
AnithaPal <anitha.aravindvel@gmail.com>
2015-08-18 11:22:32 -0400

    welcome controller added

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,22 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def policies
+  end
+
+  def insurance
+  end
+
+  def services
+  end
+
+  def contact
+  end
+
+  def resources
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+
+
+
+
+  get 'about' => 'welcome#about'
+  get 'policies' => 'welcome#policies'
+  get 'insurance' => 'welcome#insurance'
+  get 'services' => 'welcome#services'
+  get 'contact' => 'welcome#contact'
+  get 'resources' => 'welcome#resources'
+
+  root to:'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
