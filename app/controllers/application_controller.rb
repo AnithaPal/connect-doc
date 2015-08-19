ee1aec620a5b59bd8906a0066913a7f703dd3b5d
AnithaPal <anitha.aravindvel@gmail.com>
2015-08-19 10:36:09 -0400

    configuring activeadmin and devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def authenticate_active_admin_user!
+    authenticate_user!
+    unless current_user.admin?
+      flash[:alert] = "Unauthorized Access!"
+      redirect_to root_path
+    end
+  end
+
+
+  def admin?
+    role == admin
+  end
  
    protected
  
--- db/migrate/20150819141741_add_role_to_users.rb
+++ db/migrate/20150819141741_add_role_to_users.rb
@@ -2,5 +2,12 @@ class AddRoleToUsers < ActiveRecord::Migration
   def change
     add_column :users, :role, :string
     add_column :users, :name, :string
+
+    User.create! do |u|
+        u.name      = 'Admin'
+        u.email     = 'admin@connect-doc.com'
+        u.password    = 'connectdoc2015!'
+        u.role = 'admin'
+    end
   end
 end
